0683f4fd3dd3497101f88e8bc56ef5a2b1728dd3
Ravi Wu <raviwu@gmail.com>
2017-02-12 10:25:38 +0800

    finish ListsAndRecursion6.exs

--- exercises/lists_and_recursion_6.exs
+++ exercises/lists_and_recursion_6.exs
@@ -1,6 +1,7 @@
 defmodule MyList do
   def flatten([]), do: []
-  def flatten([head | tail]) when is_list(head) do
-
-  end
+  def flatten([head | tail]) when is_list(head) and is_list(tail), do: flatten(head) ++ flatten(tail)
+  def flatten([head | tail]) when (is_list(head) == false) and is_list(tail), do: [head] ++ flatten(tail)
+  def flatten([head | tail]) when is_list(head) and (is_list(tail) == false), do: flatten(head) ++ [tail]
+  def flatten([head | tail]), do: [head, tail]
 end
